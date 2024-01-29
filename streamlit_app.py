950187afc010dc8160b4bbfc2c58aa00f795dec1
sharleneraj <116897577+sharleneraj@users.noreply.github.com>
2024-01-29 15:39:35 +0530

    Update streamlit_app.py
    Added few menu details

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,9 @@
 import streamlit
-streamlit.title('Hello! This is my new Streamlit app')
+streamlit.title('My Parents New Healthy Dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale,Spinach and Rockt Smoothie')   
+streamlit.text('🐔 Hard-Boiled Free-Range egg')
+streamlit.text('🥑🍞 Avacoda Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
