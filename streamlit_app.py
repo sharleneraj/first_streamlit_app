992b527910af2c263e9b060c0ab0ec7a67cf191f
sharleneraj <116897577+sharleneraj@users.noreply.github.com>
2024-01-29 16:16:34 +0530

    Update streamlit_app.py
    New app

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,6 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale,Spinach and Rockt Smoothie')   
 streamlit.text('🐔 Hard-Boiled Free-Range egg')
 streamlit.text('🥑🍞 Avacoda Toast')
-
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas
